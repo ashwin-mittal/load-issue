62d3135c51a303b85887b866629f14818294abdf
ashwin-mittal <ashwin.mittal2553@gmail.com>
2024-04-03 18:21:15 +0530

    initial push

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // ISSUE: This is not triggering if the JavaScript resource fails to load
+  mainWindow.webContents.on('did-fail-load', (...args) => {
+    console.log('Loading failed:', ...args);
+  });
+
+  const url = (new URL('file://' + path.join(__dirname, 'index.html'))).href;
+  mainWindow.loadURL(url);
+
+  mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
